50f8380bcd701fe4c668555d40de6e3386b09ee9
Dinesh442000 <dchoudhary442000@gmail.com>
2024-02-24 17:06:41 +0530

    psh

--- src/Locators2.java
+++ src/Locators2.java
@@ -9,19 +9,36 @@ public class Locators2 {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		String name = "rahul";
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\DChoudhary5\\Documents\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		String password = getPassword(driver);
 		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
-		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
 		
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("submit")).click();
 		Thread.sleep(1000);
 		System.out.print(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello "+ name+",");
+		
+		driver.findElement(By.xpath("//button[text()= 'Log Out']")).click();
+		driver.close();
+		
+	}
+	public static String getPassword(WebDriver driver) throws InterruptedException {
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+		String [] passwordArray = passwordText.split("'");
+		String password = passwordArray[1].split("'")[0];
+		System.out.print(password);
+		return password;
 	}
 
 }
